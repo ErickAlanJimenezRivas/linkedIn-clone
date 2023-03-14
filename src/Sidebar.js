8efdc7ce382028213fc00b84b8dc2557dce42507
Alan Rivas <massimo18x@hotmail.com>
2023-03-14 13:26:10 -0400

    sidebar terminado

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,16 +3,24 @@ import React from 'react'
 import "./Sidebar.css";
 
 function Sidebar() {
+
+    const recentItem = (topic) => (
+        <div className='sidebar__recentItem'>
+            <span className='sidebar__hash'>#</span>
+            <p>{topic}</p>
+        </div>
+    )
+
   return (
     <div className='sidebar'>
         <div className='sidebar__top'>
-            <img src='' alt=''/>
+            <img src='https://images.unsplash.com/photo-1678731574077-181a363ff8f0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2533&q=80' alt=''/>
             <Avatar className='sidebar__avatar'/>
             <h2>Erick Jimenez</h2>
             <h4>erickalanjimenezrivas@gmail.com</h4>
         </div>
 
-        <div className='sidebar__status'>
+        <div className='sidebar__stats'>
             <div className='sidebar__stat'>
                 <p>Who viewed you</p>
                 <p className='sidebar__statNumber'>2,538</p>
@@ -25,6 +33,11 @@ function Sidebar() {
 
         <div className='sidebar__bottom'>
             <p>Recent</p>  
+            {recentItem('reactjs')}
+            {recentItem('programming')}
+            {recentItem('softwareengineering')}
+            {recentItem('disng')}
+            {recentItem('reactjs')}
         </div>
     </div>
   )
