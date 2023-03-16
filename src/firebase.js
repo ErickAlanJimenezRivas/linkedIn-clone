f78c06b63bb23dfb06817eabb816ad5f72b46b4d
Alan Rivas <massimo18x@hotmail.com>
2023-03-16 12:20:02 -0400

    add firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAUI6J67BNe3NfXdjxUsNM7YCtfO_23OYc",
+    authDomain: "linkedin-clone-888a5.firebaseapp.com",
+    projectId: "linkedin-clone-888a5",
+    storageBucket: "linkedin-clone-888a5.appspot.com",
+    messagingSenderId: "1768056401",
+    appId: "1:1768056401:web:330de3a120118485defa70"
+  };
