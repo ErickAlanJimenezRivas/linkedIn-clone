71f1b9db96be2118572c2db0f7f0b2c7b1d8912f
Alan Rivas <massimo18x@hotmail.com>
2023-03-16 12:31:21 -0400

    update firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
     apiKey: "AIzaSyAUI6J67BNe3NfXdjxUsNM7YCtfO_23OYc",
     authDomain: "linkedin-clone-888a5.firebaseapp.com",
@@ -6,3 +8,9 @@ const firebaseConfig = {
     messagingSenderId: "1768056401",
     appId: "1:1768056401:web:330de3a120118485defa70"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { db, auth };
