cf23026beb9c3dda3f69c2e979b0fe4e123bd62d
Alan Rivas <massimo18x@hotmail.com>
2023-03-21 12:10:29 -0400

    details

--- src/Header.js
+++ src/Header.js
@@ -9,11 +9,12 @@ import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
 import ChatIcon from '@mui/icons-material/Chat';
 import NotificationsIcon from '@mui/icons-material/Notifications';
-import { useDispatch } from 'react-redux';
-import { logout } from './features/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, selectUser } from './features/userSlice';
 import { auth } from './firebase';
 
 function Header() {
+  const user = useSelector(selectUser);
   const dispatch = useDispatch();
 
   const logoutOfApp = () => {
@@ -40,7 +41,7 @@ function Header() {
           <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
           <HeaderOption Icon={ChatIcon} title="Messaging"/>
           <HeaderOption Icon={NotificationsIcon} title="Notifications"/>
-          <HeaderOption avatar={Avatar} title='Me' onClick={logoutOfApp}/>
+          <HeaderOption avatar={user.photoUrl} title='Me' onClick={logoutOfApp}/>
         </div>
     </div>
   )
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,11 @@
 import { Avatar } from '@mui/material';
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
 import "./Sidebar.css";
 
 function Sidebar() {
+    const user = useSelector(selectUser);
 
     const recentItem = (topic) => (
         <div className='sidebar__recentItem'>
@@ -15,9 +18,10 @@ function Sidebar() {
     <div className='sidebar'>
         <div className='sidebar__top'>
             <img src='https://images.unsplash.com/photo-1678731574077-181a363ff8f0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2533&q=80' alt=''/>
-            <Avatar className='sidebar__avatar'/>
-            <h2>Erick Jimenez</h2>
-            <h4>erickalanjimenezrivas@gmail.com</h4>
+            <Avatar src={user.photoUrl} className='sidebar__avatar'>
+                {user.email[0]}</Avatar>
+            <h2>{user.displayName}</h2>
+            <h4>{user.email}</h4>
         </div>
 
         <div className='sidebar__stats'>
